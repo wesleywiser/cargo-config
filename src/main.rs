59533e5b9521de94642a54f5643eff7419beb2d7
luozijun <luozijun.assistant@gmail.com>
2020-02-07 12:00:48 +0800

    Added handling of `.cargo/config` files

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,127 @@
-// Copyright 2015 Wesley Wiser. 
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
-extern crate rustc_serialize;
 extern crate toml;
+extern crate serde_json;
 
-use std::env::args;
-use std::fs::File;
-use std::io::Read;
-use std::process::Command;
-use rustc_serialize::json::Json;
-use toml::{Parser, Value};
+use std::fs;
+use std::io;
+use std::env;
+use std::path::PathBuf;
 
-fn main() {
-    let toml_path = get_cargo_toml_path();
-    let mut toml_file = 
-        match File::open(toml_path) {
-            Ok(f) => f,
-            Err(_) => panic!("Couldn't open Cargo.toml")
-        };
 
-    let mut contents = String::new();
-    toml_file.read_to_string(&mut contents).unwrap();
+fn run(args: &[&str]) -> Result<String, io::Error> {
+    let args_len = args.len();
 
-    let mut parser = Parser::new(&*contents);
-    let toml_table = Value::Table(parser.parse().unwrap());
-
-    let toml_value_path = get_requested_path();
-    let value = toml_table.lookup(&*toml_value_path).expect("Couldn't find the requested value");
-
-    println!("{}", value);
-}
-
-fn get_requested_path() -> String {
-    let arguments = args();
-
-    if arguments.len() != 3 {
-        panic!("Usage cargo config path.to.value");
+    if args_len == 0 {
+        return Err(io::Error::from(io::ErrorKind::NotFound));
     }
 
-    arguments.last().unwrap()
+    let mut cmd = std::process::Command::new(&args[0]);
+
+    if args_len > 1 {
+        cmd.args(&args[1..]);
+    }
+
+    let output = cmd.output()?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+        Err(io::Error::new(io::ErrorKind::Other, stderr))
+    }
 }
 
-fn get_cargo_toml_path() -> String {
-    let output = cargo("locate-project");
-    let json = 
-        match Json::from_str(&*output) {
-            Ok(j) => j,
-            Err(_) => panic!("Couldn't parse the output of `cargo locate-project`")
-        };
-    json["root"].as_string().unwrap().to_string()
+fn query_manifest_path() -> Result<String, Box<dyn std::error::Error>> {
+    let output = run(&["cargo", "locate-project"])?;
+
+    let value = serde_json::from_str::<serde_json::Value>(&output)?;
+    
+    match value {
+        serde_json::Value::Object(obj) => {
+            match obj.get("root") {
+                Some(val) => {
+                    match val {
+                        serde_json::Value::String(s) => Ok(s.clone()),
+                        _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid manifest json input").into()),
+                    }
+                },
+                None => Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid manifest json input").into()),
+            }
+        },
+        _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid manifest json input").into()),
+    }
 }
 
-fn cargo(command: &str) -> String {
-    let output = Command::new("cargo")
-                         .arg(command)
-                         .output()
-                         .unwrap();
-    String::from_utf8_lossy(&output.stdout).into_owned()
+fn table_query<'a>(value: &'a toml::Value, query: &str) -> Option<&'a toml::Value> {
+    match value {
+        &toml::Value::Table(ref table) => table.get(query),
+        _ => None,
+    }
 }
 
+fn lookup<'a>(mut value: &'a toml::Value, keys: &str) -> Option<&'a toml::Value> {
+    let mut founded = false;
+    for key in keys.split(".") {
+        match table_query(&value, key) {
+            Some(val) => {
+                value = val;
+                if !founded {
+                    founded = true;
+                }
+            },
+            None => break,
+        }
+    }
+
+    if founded {
+        Some(value)
+    } else {
+        None
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args();
+    args.next();
+    args.next();
+
+    let mut toml_paths = vec![ PathBuf::from(query_manifest_path()?) ];
+
+    if let Some(project_dir) = &toml_paths[0].parent() {
+        let p1 = project_dir.join(".cargo/config");
+        let p2 = project_dir.join(".cargo/config.toml");
+
+        if p1.exists() && p1.is_file() {
+            toml_paths.push(p1);
+        } else if p2.exists() && p2.is_file() {
+            toml_paths.push(p2);
+        }
+
+        if let Ok(cargo_home_dir) = env::var("CARGO_HOME") {
+            let cargo_home_path = PathBuf::from(cargo_home_dir);
+            let p1 = cargo_home_path.join("config");
+            let p2 = cargo_home_path.join("config.toml");
+
+            if p1.exists() && p1.is_file() {
+                toml_paths.push(p1);
+            } else if p2.exists() && p2.is_file() {
+                toml_paths.push(p2);
+            }
+        }
+    }
+
+    if let Some(args) = args.next() {
+        println!("KEYS: {:?}", args);
+        for toml_path in toml_paths.iter() {
+            let content = fs::read_to_string(toml_path)?;
+            let toml_value = toml::from_str::<toml::Value>(&content)?;
+
+            if let Some(val) = lookup(&toml_value, &args) {
+                println!("TOML_FILE: {:?}", toml_path);
+                println!("TOML_VALUE:\n{}", val);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
